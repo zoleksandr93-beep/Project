a8a63f569e1878d9e1a190701cf4578b15c677f5
zoleksandr93-beep <zoleksandr93@gmail.com>
2025-11-14 23:32:55 +0200

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,16 +1,23 @@
-#include <iostream>   
-#include <math.h>     //підключення бібліотек
+#include<iostream>
+#include <math.h>
 using namespace std;
-int main()
-{
-    double d, z, a;             //Ввід змінних
-    cout << "Vvedit d:";        
-    cin >> d;
-    cout << "Vvedit z:"/endl;
-    cin >> z;
-    cout << "Vvedit a:";
-    cin >> a;
-    double b = d * (atan(z) + exp(-(d + 3)));
-    cout << "Vdpovid: b=" << b << endl;  
+int main() {
+	double x, z;
+    cout << "Vvedit  x: ";
+    cin >> x;
+    if (x <= 0) {
+        z = x + 3;
+    }
+    else if (x > 0 && x < 1.5) {
+        z = x * sin(3 * x);
+    }
+    else if (x >= 1.5 && x <= 2.5) {
+        z = pow(x, 2) - 1;
+    }
+    else {
+        z = x - 5;
+    }
+    cout << "Rezyltat: z = " << z << endl;
     return 0;
 }
+
